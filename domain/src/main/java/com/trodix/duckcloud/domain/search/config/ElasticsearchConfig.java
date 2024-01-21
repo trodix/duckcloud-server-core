fe1747ed23d677354d07e0b06df86afff7eb9580
Sebastien Vallet <sebastien.vallet89@gmail.com>
2024-01-21 10:19:24 +0100

    fix java time module serialization elasticsearch

--- domain/src/main/java/com/trodix/duckcloud/domain/search/config/ElasticsearchConfig.java
+++ domain/src/main/java/com/trodix/duckcloud/domain/search/config/ElasticsearchConfig.java
@@ -1,6 +1,15 @@
 package com.trodix.duckcloud.domain.search.config;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.JsonpMapper;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
@@ -17,4 +26,29 @@ public class ElasticsearchConfig extends ElasticsearchConfiguration {
 			.connectedTo(elasticsearchUrl)
 			.build();
 	}
+
+	@Bean
+	public ElasticsearchClient elasticsearchClient(RestClient restClient, JsonpMapper jsonpMapper) {
+		RestClientTransport transport = new RestClientTransport(restClient, jsonpMapper);
+		return new ElasticsearchClient(transport);
+	}
+
+	@Bean
+	public JsonpMapper jacksonJsonpMapper(ObjectMapper objectMapper) {
+		ObjectMapper elasticSearchobjectMapper = makeElasticCompatible(objectMapper);
+
+		// add java 8 date serialization / deserialization feature
+		elasticSearchobjectMapper.registerModule(new JavaTimeModule());
+
+		return new JacksonJsonpMapper(elasticSearchobjectMapper);
+	}
+
+	private ObjectMapper makeElasticCompatible(ObjectMapper objectMapper) {
+		if (!objectMapper.isEnabled(SerializationFeature.INDENT_OUTPUT)) {
+			return objectMapper;
+		}
+
+		return objectMapper.copy().disable(SerializationFeature.INDENT_OUTPUT);
+	}
+
 }
